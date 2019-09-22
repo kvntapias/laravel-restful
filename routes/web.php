cc18634e0d1e8ccb652934ca882073c475f119f6
kevin <kevin-1134@live.com>
2019-09-22 16:22:48 -0500

    rutas actualizadas

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('api/register', 'UserController@register');
-Route::post('api/login', 'UserController@login');
-Route::put('api/user/update', 'UserController@update');
+Route::post('/api/register', 'UserController@register');
+Route::post('/api/login', 'UserController@login');
+Route::put('/api/user/update', 'UserController@update');
 Route::post('api/user/upload', 'UserController@upload')
     ->middleware(ApiAuthMiddleware::class);
 Route::get('/api/user/avatar/{filename}', 'UserController@getImage');
@@ -18,7 +18,7 @@ Route::resource('/api/category', 'CategoryController');
 
 //RUTAS CONTROLADOR POSTS
 Route::resource('/api/post', 'PostController');
-Route::post('api/post/upload', 'PostController@upload');
+Route::post('/api/post/upload', 'PostController@upload');
 Route::get('/api/post/image/{filename}', 'PostController@getImage');
 Route::get('/api/post/category/{id}', 'PostController@getPostsByCategory');
 Route::get('/api/post/user/{id}', 'PostController@getPostsByUser');
